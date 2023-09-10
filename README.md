73b2cde1c6e0bf392938a04caaef2451984ee363
Ben Rucker <brucker117@gmail.com>
2023-09-10 19:50:54 -0400

    Add previous result check

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # sf-globalentry-watcher
+
 A simple script to check for SFO GlobalEntry interview timeslots.
+
+## Setup
+
+1. `npm install`
+2. `npm run dev` or `ts-node-esm src/index`
+
+## Cron setup
+
+1. Add a file called `cron-runnable.sh` with these contents:
+
+```sh
+#!/usr/bin/env sh
+PATH=PASTE_PATH_HERE
+npm run dev
+```
+
+2. Run `echo $PATH`, copy and paste the output to the second line of `cron-runnable.sh`
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,55 @@
 import "dotenv/config";
 import fetch, { Headers } from "node-fetch";
+import { readFile, writeFile } from "fs/promises";
 import { TimeSlot } from "./types/TimeSlot.js";
 import { TimeSlotsResponse } from "./types/TimeSlotsResponse.js";
 
 const SFO_API_URL =
   "https://ttp.cbp.dhs.gov/schedulerapi/slot-availability?locationId=5446";
 
+const RESULT_FILENAME = "lastResult";
+
+const EMPTY_RESULTS_STRING = "{}";
+
 export async function main() {
   const timeSlots = await fetchTimeSlots();
-  await sendDiscordMessage(buildWebhookRequestBody(timeSlots));
+  const haveResultsChanged = await compareResults(timeSlots);
+  if (haveResultsChanged) {
+    await sendDiscordMessage(buildWebhookRequestBody(timeSlots));
+  } else {
+    console.log("No change since last run");
+  }
 }
 
 async function fetchTimeSlots() {
   const request = await fetch(SFO_API_URL);
   const response = await request.json();
   return TimeSlotsResponse.check(response);
+}
+
+async function compareResults(newTimeSlots: TimeSlotsResponse) {
+  const firstTimeSlot = newTimeSlots.availableSlots[0];
+  const newResult =
+    firstTimeSlot != null
+      ? JSON.stringify(firstTimeSlot)
+      : EMPTY_RESULTS_STRING;
+
+  await touchResultsFile();
+
+  const lastResult = (await readFile(RESULT_FILENAME)).toString();
+  console.log("last", lastResult);
+  console.log("new", newResult);
+  if (newResult !== lastResult) {
+    await writeFile(RESULT_FILENAME, newResult);
+    return true;
+  }
+  return false;
+}
+
+async function touchResultsFile() {
+  try {
+    await writeFile(RESULT_FILENAME, EMPTY_RESULTS_STRING, { flag: "wx" });
+  } catch (error) {}
 }
 
 async function sendDiscordMessage(content: string) {
